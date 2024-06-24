33df82e86fce40a975a2914502ce206140001552
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-06-24 20:31:34 +0900

    feat: id를 통한 상품 조회 기능 개발
    id를 통해 상품을 조회하며, 만약 존재하지 않는 id가 입력되었을 경우 NotFoundProductException 예외를 발생시킨다.

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -16,18 +16,26 @@ public class ProductController {
     @PutMapping("/add")
     public Product addProduct(@RequestParam String name, @RequestParam int price, @RequestParam String imageUrl) {
         var product = new Product(count, name, price, imageUrl);
-        products.put(count++,product);
+        products.put(count++, product);
         return product;
     }
 
     @PatchMapping("/update/{id}")
-    public Product updateProduct(@PathVariable Long id,@RequestParam String name, @RequestParam int price, @RequestParam String imageUrl) {
-        if(!products.containsKey(id)){
-            throw new NotFoundProductException(id+"를 가진 상품이 존재하지 않습니다.");
+    public Product updateProduct(@PathVariable Long id, @RequestParam String name, @RequestParam int price, @RequestParam String imageUrl) {
+        if (!products.containsKey(id)) {
+            throw new NotFoundProductException(id + "를 가진 상품이 존재하지 않습니다.");
         }
         var product = new Product(id, name, price, imageUrl);
 
-        products.put(id,product);
+        products.put(id, product);
         return product;
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        if (!products.containsKey(id)) {
+            throw new NotFoundProductException(id + "를 가진 상품이 존재하지 않습니다.");
+        }
+        return products.get(id);
     }
 }
