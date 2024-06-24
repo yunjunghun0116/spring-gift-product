69a4faa103b515b0f0eca08257c28dabec5f990e
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-06-24 21:49:25 +0900

    refactor: 상품의 잘못된 id 접근 예외처리를 함수로 추출
    존재하지 않는 id를 통해 상품에 접근을 시도할 때 예외를 발생시키는 부분을 함수로 추출하는 작업을 수행

--- src/main/java/gift/ProductController.java
+++ src/main/java/gift/ProductController.java
@@ -8,7 +8,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController("/api/products")
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
 
     private final Map<Long, Product> products = new HashMap<>();
@@ -24,9 +25,7 @@ public class ProductController {
 
     @PatchMapping("/update/{id}")
     public Product updateProduct(@PathVariable Long id, @RequestParam String name, @RequestParam int price, @RequestParam String imageUrl) {
-        if (!products.containsKey(id)) {
-            throw new NotFoundProductException(id + "를 가진 상품이 존재하지 않습니다.");
-        }
+        productIdValidation(id);
         var product = new Product(id, name, price, imageUrl);
 
         products.put(id, product);
@@ -35,9 +34,7 @@ public class ProductController {
 
     @GetMapping("/{id}")
     public Product getProductById(@PathVariable Long id) {
-        if (!products.containsKey(id)) {
-            throw new NotFoundProductException(id + "를 가진 상품이 존재하지 않습니다.");
-        }
+        productIdValidation(id);
         return products.get(id);
     }
 
@@ -48,9 +45,17 @@ public class ProductController {
 
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable Long id) {
+        productIdValidation(id);
+        products.remove(id);
+    }
+
+    /**
+     * @param id
+     * 만약 존재하지 않는 id를 통해 객체에 접근을 시도할 경우 NotFoundProductException 예외를 발생시킨다.
+     */
+    public void productIdValidation(Long id){
         if (!products.containsKey(id)) {
             throw new NotFoundProductException(id + "를 가진 상품이 존재하지 않습니다.");
         }
-        products.remove(id);
     }
 }
