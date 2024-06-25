3083ca5ea09cdf045f568ad618d94aa319880d82
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-06-25 15:34:13 +0900

    add: 테스트 케이스 생성하기
    다양한 경우에 해당하는 테스트 케이스 생성

---
+++ src/test/java/gift/service/ProductServiceTest.java
@@ -0,0 +1,46 @@
+package gift.service;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @AfterAll
+    @DisplayName("상품 레포지토리 초기화하기")
+    void clearAll(){
+
+    }
+
+    @Test
+    @DisplayName("정상 상품 추가하기")
+    void addProductSuccess() {
+    }
+
+    @Test
+    @DisplayName("오류 상품 추가하기")
+    void addProductFail() {
+    }
+
+    @Test
+    @DisplayName("상품 수정하기")
+    void updateProduct() {
+    }
+
+    @Test
+    @DisplayName("하나의 상품 조회하기")
+    void getProduct() {
+    }
+
+    @Test
+    @DisplayName("전체 상품 조회하기")
+    void getProducts() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+}
