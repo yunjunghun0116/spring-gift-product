96c523535bcb842c9ca5e8f69a68818872100054
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-06-25 16:39:00 +0900

    change: 컨트롤러 계층에서의 데이터 전달을 객체 -> ResponseEntity로 변경
    컨트롤러 계층에서의 데이터 전달을 객체에서 ResponseEntity로 변경

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -3,6 +3,7 @@ package gift.controller;
 import gift.model.Product;
 import gift.dto.ProductDto;
 import gift.service.ProductService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,29 +19,32 @@ public class ProductController {
     }
 
     @PutMapping("/add")
-    public Product addProduct(@RequestBody ProductDto productDto) {
+    public ResponseEntity<Product> addProduct(@RequestBody ProductDto productDto) {
         var product = service.addProduct(productDto);
-        return product;
+        return ResponseEntity.ok(product);
     }
 
     @PatchMapping("/update/{id}")
-    public Product updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto) {
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto) {
         var product = service.updateProduct(id, productDto);
-        return product;
+        return ResponseEntity.ok(product);
     }
 
     @GetMapping("/{id}")
-    public Product getProduct(@PathVariable Long id) {
-        return service.getProduct(id);
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        var product = service.getProduct(id);
+        return ResponseEntity.ok(product);
     }
 
     @GetMapping
-    public List<Product> getProducts() {
-        return service.getProducts();
+    public ResponseEntity<List<Product>> getProducts() {
+        var products = service.getProducts();
+        return ResponseEntity.ok(products);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         service.deleteProduct(id);
+        return ResponseEntity.ok().build();
     }
 }
