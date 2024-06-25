86aef8dfc2609efb881748b1a3a925d74d5e6e74
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-06-25 15:56:41 +0900

    add: 테스트 케이스 작성
    테스트 케이스 작성

--- src/test/java/gift/service/ProductServiceTest.java
+++ src/test/java/gift/service/ProductServiceTest.java
@@ -1,46 +1,71 @@
 package gift.service;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import gift.dto.ProductDto;
+import gift.exception.PriceLessThanZeroException;
+import gift.model.Product;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+@SpringBootTest
 class ProductServiceTest {
 
-    @AfterAll
-    @DisplayName("상품 레포지토리 초기화하기")
-    void clearAll(){
+    @Autowired
+    ProductService service;
 
+    @AfterEach
+    @DisplayName("상품 레포지토리 초기화하기")
+    void clearAll() {
+        List<Product> products = service.getProducts();
+        for (var product : products) {
+            service.deleteProduct(product.getId());
+        }
     }
 
     @Test
     @DisplayName("정상 상품 추가하기")
     void addProductSuccess() {
+        ProductDto productDto = new ProductDto("상품1", 10000, "이미지 주소");
+        Product savedProduct = service.addProduct(productDto);
+        Assertions.assertThat(savedProduct.getName()).isEqualTo("상품1");
     }
 
     @Test
-    @DisplayName("오류 상품 추가하기")
+    @DisplayName("오류 상품 생성하기")
     void addProductFail() {
+        Assertions.assertThatThrownBy(() -> new ProductDto("상품1", -1000, "이미지 주소"))
+                .isInstanceOf(PriceLessThanZeroException.class);
     }
 
     @Test
     @DisplayName("상품 수정하기")
     void updateProduct() {
+        ProductDto productDto = new ProductDto("상품1", 10000, "이미지 주소");
+        Product savedProduct = service.addProduct(productDto);
+
+        Long id = savedProduct.getId();
+        ProductDto updateDto = new ProductDto("상품1", 7000, "이미지 주소2");
+
+        service.updateProduct(id, updateDto);
+
+        Product updatedProduct = service.getProduct(id);
+        Assertions.assertThat(updatedProduct.getPrice()).isEqualTo(7000);
     }
 
     @Test
-    @DisplayName("하나의 상품 조회하기")
-    void getProduct() {
-    }
-
-    @Test
-    @DisplayName("전체 상품 조회하기")
-    void getProducts() {
-    }
-
-    @Test
+    @DisplayName("상품 삭제하기")
     void deleteProduct() {
+        ProductDto productDto = new ProductDto("상품1", 10000, "이미지 주소");
+        Product savedProduct = service.addProduct(productDto);
+
+        Assertions.assertThat(service.getProducts().size()).isEqualTo(1);
+
+        Long id = savedProduct.getId();
+        service.deleteProduct(id);
+
+        Assertions.assertThat(service.getProducts().size()).isEqualTo(0);
     }
 }
