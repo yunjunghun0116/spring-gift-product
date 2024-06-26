f85b5c8c707d47e680f7b356e52997fa89772711
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-06-26 15:09:44 +0900

    feat: 타임리프를 사용한 관리자페이지 연결
    타임리프를 사용하여 관리자페이지를 연결

---
+++ src/main/java/gift/controller/AdminController.java
@@ -0,0 +1,17 @@
+package gift.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping
+    public String getAdminMain(Model model){
+        model.addAttribute("data","관리자");
+        return "admin";
+    }
+}
