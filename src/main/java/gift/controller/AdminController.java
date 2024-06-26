8a2092c2f978d3838f944b6c29036b4f7f95734c
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-06-26 16:05:02 +0900

    feat: 관리자 전용 컨트롤러에서 상품 목록에 접근할 수 있는 기능 및 타임리프 연결
    관리자 전용 컨트롤러에서 상품 목록에 접근할 수 있는 기능 및 타임리프 연결

--- src/main/java/gift/controller/AdminController.java
+++ src/main/java/gift/controller/AdminController.java
@@ -1,17 +1,52 @@
 package gift.controller;
 
+import gift.dto.ProductDto;
+import gift.model.Product;
+import gift.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/admin")
+@RequestMapping("/products")
 public class AdminController {
 
+    ProductService service;
+
+    public AdminController(ProductService service) {
+        this.service = service;
+    }
+
     @GetMapping
-    public String getAdminMain(Model model){
-        model.addAttribute("data","관리자");
-        return "admin";
+    public String getProducts(Model model) {
+        List<Product> products = service.getProducts();
+        model.addAttribute("data", "관리자");
+        model.addAttribute("products", products);
+        return "views/products";
+    }
+
+    @GetMapping("/{id}")
+    public String getProduct(@PathVariable Long id, Model model) {
+        Product product = service.getProduct(id);
+        model.addAttribute("product", product);
+        return "views/product";
+    }
+
+    @GetMapping("/add")
+    public String getAddForm() {
+        return "views/addProduct";
+    }
+
+    @PostMapping("/add")
+    public String save(@ModelAttribute ProductDto productDto, RedirectAttributes redirectAttributes) {
+        Product product = service.addProduct(productDto);
+
+        redirectAttributes.addAttribute("productId", product.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/products/{productId}";
     }
 }
