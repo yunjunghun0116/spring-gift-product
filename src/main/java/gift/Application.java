9297ed36530556edf1452e88d91f8d65f5f99f69
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-07-01 23:49:29 +0900

    refactor: disable wildcard import 적용
    자동으로 .*로 인해 wildcard import 되는 부분을 intellij 설정을 바꿈과 함께 모두 찾아 제거

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -1,9 +1,7 @@
 package gift;
 
-import gift.config.ProductConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class Application {
